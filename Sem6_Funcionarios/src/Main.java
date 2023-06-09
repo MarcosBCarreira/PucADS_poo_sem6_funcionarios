730feee9264df80289a6ae7a391df4d72096eee3
Marcos B. Carreira <120515145+MarcosBCarreira@users.noreply.github.com>
2023-06-09 00:22:36 -0300

    Ajustes mínimos de formato

--- Sem6_Funcionarios/src/Main.java
+++ Sem6_Funcionarios/src/Main.java
@@ -3,8 +3,8 @@ import java.util.ArrayList;
 public class Main {
 	public static void main(String[] args) {
 		var funcionarios = new ArrayList<Funcionario>();
-		funcionarios.add(new Concursado("João", 4000, 5)); // João
-		funcionarios.add(new Temporario("José", 2000, 12)); // José
+		funcionarios.add(new Concursado("João", 4000, 5 ));   // João
+		funcionarios.add(new Temporario("José", 2000, 12 ));  // José
 		funcionarios.add(new Concursado("Maria", 2400, 34)); // Maria
 		funcionarios.add(new Temporario("Pedro", 1200, 10)); // Pedro
 
@@ -14,5 +14,7 @@ public class Main {
 					(funcionario instanceof Temporario ? (funcionario.getTempoServico() + " meses de contrato.") : (funcionario.getTempoServico() 
 							+ " anos de serviço.")));
 		}
+	
 	}
+
 }
